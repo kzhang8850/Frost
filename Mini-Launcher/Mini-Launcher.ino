44590fce367038ebd095255cef30e4a9e6895615
Kevin Zhang <kzhang@ivani.com>
2016-10-19 21:48:56 -0400

    got input working, need to clean up code

--- Mini-Launcher/Mini-Launcher.ino
+++ Mini-Launcher/Mini-Launcher.ino
@@ -1,7 +1,14 @@
 int angle;
 int power;
-
+int counter;
+char separator[] = ",e";
+int inputs[2];
+int i = 0;
+char *token = "e";
 String input;
+String angle2;
+String power2;
+boolean received = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,16 +21,32 @@ void setup() {
 
 void loop() {
   
-  if input != "default"){
-    Serial.print("Current angle: ");
+  if (received){
+    Serial.print("Current Angle: ");
     Serial.println(angle);
     Serial.print("Current Power: ");
     Serial.println(power);
+    received = false;
    }
 
 }
 
 void serialEvent(){
-  input = Serial.readString();
+  if(Serial.available()){
+    input = Serial.readString();
+    Serial.print("captured String is : "); 
+    Serial.println(input); //prints string to serial port out
+    Serial.println(input);
+    int len = input.length()+1;
+    char realinput[len];
+    input.toCharArray(realinput, len);
+    Serial.println(realinput[1]);
+
+    counter = input.indexOf(",");
+    angle = input.substring(0,counter).toInt();
+    power = input.substring(counter+1,len).toInt();
+
+    received = true;
+  }
 }
 
