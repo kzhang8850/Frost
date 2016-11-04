2ac5757ee42b3c8d2e75d548fc9aa831e3c359bb
Kevin Zhang <kzhang@ivani.com>
2016-11-04 00:23:00 -0400

    fiddling with various traking methods

--- tracking.py
+++ tracking.py
@@ -1,22 +1,28 @@
-from __future__ import print_function
-from imutils.object_detection import non_max_suppression
-from imutils import paths
 import numpy as np
-import argparse
-import imutils
 import cv2
 
-cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
+cap = cv2.VideoCapture(0)
+fgbg = cv2.BackgroundSubtractorMOG()
+while(1):
+
     ret, frame = cap.read()
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+
+    fgmask = fgbg.apply(frame)
+
+
+
+    cv2.imshow('frame',fgmask)
+
+    k = cv2.waitKey(30) & 0xff
+
+    if k == 27:
+
         break
 
-# When everything done, release the capture
+
+
 cap.release()
 cv2.destroyAllWindows()
