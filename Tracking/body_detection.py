48bafecf53b9eeee063b4309295ec8819e3130b0
Kevin Zhang <kzhang@ivani.com>
2016-11-29 12:01:45 -0500

    made it OOP

--- Tracking/body_detection.py
+++ Tracking/body_detection.py
@@ -2,151 +2,36 @@ import numpy as np
 import cv2
 
 
-# Malisiewicz et al.
-def non_max_suppression_fast(boxes, overlapThresh):
-	# if there are no boxes, return an empty list
-	if len(boxes) == 0:
-		return []
 
-	# if the bounding boxes integers, convert them to floats --
-	# this is important since we'll be doing a bunch of divisions
-	if boxes.dtype.kind == "i":
-		boxes = boxes.astype("float")
+class BodyDetector(object):
+	def __init__(self):
+		self.winStride = (4,4)
+		self.padding = (16,16)
+		self.scale = 1.03
+		self.meanShift = False
 
-	# initialize the list of picked indexes
-	pick = []
+		self.cam = cv2.VideoCapture(0)
+		self.hog = cv2.HOGDescriptor()
+		self.history = []
+		self.people_ranges = []
 
-	# grab the coordinates of the bounding boxes
-	x1 = boxes[:,0]
-	y1 = boxes[:,1]
-	x2 = boxes[:,2]
-	y2 = boxes[:,3]
+		self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-	# compute the area of the bounding boxes and sort the bounding
-	# boxes by the bottom-right y-coordinate of the bounding box
-	area = (x2 - x1 + 1) * (y2 - y1 + 1)
-	idxs = np.argsort(y2)
-
-	# keep looping while some indexes still remain in the indexes
-	# list
-	while len(idxs) > 0:
-		# grab the last index in the indexes list and add the
-		# index value to the list of picked indexes
-		last = len(idxs) - 1
-		i = idxs[last]
-		pick.append(i)
-
-		# find the largest (x, y) coordinates for the start of
-		# the bounding box and the smallest (x, y) coordinates
-		# for the end of the bounding box
-		xx1 = np.maximum(x1[i], x1[idxs[:last]])
-		yy1 = np.maximum(y1[i], y1[idxs[:last]])
-		xx2 = np.minimum(x2[i], x2[idxs[:last]])
-		yy2 = np.minimum(y2[i], y2[idxs[:last]])
-
-		# compute the width and height of the bounding box
-		w = np.maximum(0, xx2 - xx1 + 1)
-		h = np.maximum(0, yy2 - yy1 + 1)
-
-		# compute the ratio of overlap
-		overlap = (w * h) / area[idxs[:last]]
-
-		# delete all indexes from the index list that have
-		idxs = np.delete(idxs, np.concatenate(([last],
-			np.where(overlap > overlapThresh)[0])))
-
-	# return only the bounding boxes that were picked using the
-	# integer data type
-	return boxes[pick].astype("int")
-
-
-def find_targets(people):
-	lidar_readings = {}
-	target_readings = {}
-	readings_list = []
-	targeted = []
-	threshold = 2
-	while len(lidar_readings) < 360:
-		angle, distance = get_lidar_reading()
-		lidar_readings[angle] = distance
-	for person in people:
-		for ang, dist in lidar_readings.items():
-			if ang > person[0] and ang < person[1]:
-				target_readings[ang] = dist
-		readings_list = target_readings.items()
-		readings_list = sorted(readings_list, key=lambda tup: tup[0])
-		count = -1
-		comparecount = 0
-		target = None
-		comparison = readings_list[0]
-		for angle, distance in readings_list:
-			if abs(distance - comparison[1]) < 2:
-				count += 1
-			else:
-				if count > comparecount:
-					comparecount = count
-					target = comparison
-				comparison = angle, distance
-				count = 0
-
-		targeted.append(target[0] + count/2, target[1])
-
-	return targeted
-
-
-
-if __name__ == "__main__":
-
-	winStride = (4,4)
-	padding = (16,16)
-	scale = 1.03
-	meanShift = False
-
-	cap = cv2.VideoCapture(0)
-
-	hog = cv2.HOGDescriptor()
-	hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-	history = []
-	people = []
-	targets = []
-
-	while True:
-
-		ret, frame = cap.read()
+	def find_bodies(self):
+		ret, frame = self.cam.read()
 		frame = cv2.resize(frame, (320, 240))
 
+		(rects, weights) = self.hog.detectMultiScale(frame, winStride=self.winStride,
+		padding=self.padding, scale=self.scale, useMeanshiftGrouping=self.meanShift)
 
-		(rects, weights) = hog.detectMultiScale(frame, winStride=winStride,
-		padding=padding, scale=scale, useMeanshiftGrouping=meanShift)
+		self.people_ranges = self.draw_rectangles(rects,frame)
 
-		print len(rects)
-		if len(rects) == 0 and len(history) > 0 and any(len(item) > 0 for item in history):
-			print "i'm in history"
-			for i in range(len(history)-1, -1, -1):
-				if len(history[i]) > 0:
-					hist = non_max_suppression_fast(history[i], 0.5)
-					for (x, y, w, h) in hist:
-						cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-						people.append((x * 57/640, (x + w) * 57/640))
-					break
+
+		if len(self.history) < 10:
+			self.history.append(self.people_ranges)
 		else:
-			rects = non_max_suppression_fast(rects, 0.5)
-			for (x, y, w, h) in rects:
-				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-				people.append((x * 57/640, (x + w) * 57/640))
-
-		# for person in people:
-		# 	print person
-		#
-		# targets = find_targets(people)
-		# print targets
-
-		if len(history) < 10:
-			history.append(rects)
-		else:
-			history.pop(0)
-			history.append(rects)
+			self.history.pop(0)
+			self.history.append(self.people_ranges)
 		# print history
 
 		people = []
@@ -156,13 +41,154 @@ if __name__ == "__main__":
 
 		cv2.imshow('frame',frame)
 
+		return self.people_ranges
+
+
+	def draw_rectangles(self,rects, frame):
+
+		if len(rects) == 0 and len(self.history) > 0 and any(len(item) > 0 for item in self.history):
+			print "i'm in history"
+			for i in range(len(self.history)-1, -1, -1):
+				if len(self.history[i]) > 0:
+					hist = self.non_max_suppression_fast(self.history[i], 0.5)
+					for (x, y, w, h) in hist:
+						cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+					return hist
+		else:
+			rects = self.non_max_suppression_fast(rects, 0.5)
+			for (x, y, w, h) in rects:
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+			return rects
+
+
+	# Malisiewicz et al.
+	def non_max_suppression_fast(self,boxes, overlapThresh):
+		# if there are no boxes, return an empty list
+		if len(boxes) == 0:
+			return []
+
+		# if the bounding boxes integers, convert them to floats --
+		# this is important since we'll be doing a bunch of divisions
+		if boxes.dtype.kind == "i":
+			boxes = boxes.astype("float")
+
+		# initialize the list of picked indexes
+		pick = []
+
+		# grab the coordinates of the bounding boxes
+		x1 = boxes[:,0]
+		y1 = boxes[:,1]
+		x2 = boxes[:,2]
+		y2 = boxes[:,3]
+
+		# compute the area of the bounding boxes and sort the bounding
+		# boxes by the bottom-right y-coordinate of the bounding box
+		area = (x2 - x1 + 1) * (y2 - y1 + 1)
+		idxs = np.argsort(y2)
+
+		# keep looping while some indexes still remain in the indexes
+		# list
+		while len(idxs) > 0:
+			# grab the last index in the indexes list and add the
+			# index value to the list of picked indexes
+			last = len(idxs) - 1
+			i = idxs[last]
+			pick.append(i)
+
+			# find the largest (x, y) coordinates for the start of
+			# the bounding box and the smallest (x, y) coordinates
+			# for the end of the bounding box
+			xx1 = np.maximum(x1[i], x1[idxs[:last]])
+			yy1 = np.maximum(y1[i], y1[idxs[:last]])
+			xx2 = np.minimum(x2[i], x2[idxs[:last]])
+			yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+			# compute the width and height of the bounding box
+			w = np.maximum(0, xx2 - xx1 + 1)
+			h = np.maximum(0, yy2 - yy1 + 1)
+
+			# compute the ratio of overlap
+			overlap = (w * h) / area[idxs[:last]]
+
+			# delete all indexes from the index list that have
+			idxs = np.delete(idxs, np.concatenate(([last],
+				np.where(overlap > overlapThresh)[0])))
+
+		# return only the bounding boxes that were picked using the
+		# integer data type
+		return boxes[pick].astype("int")
+
+
+
+class TargetLocator(object):
+	def __init__(self):
+		self.people = []
+		self.targets = []
+		self.lidar_readings = {}
+		self.target_readings = {}
+		self.readings_list = []
+		self.targeted = []
+		self.threshold = 2
+
+	def track(self, crowd):
+		for (x, y, w, h) in crowd:
+			self.people.append((x * 57/640, (x + w) * 57/640))
+
+		for person in self.people:
+			print person
+
+		self.targets = self.find_targets()
+		print self.targets
+
+
+	def find_targets(self):
+
+		while len(self.lidar_readings) < 360:
+			angle, distance = get_lidar_reading()
+			self.lidar_readings[angle] = distance
+		for person in self.people:
+			for ang, dist in self.lidar_readings.items():
+				if ang > person[0] and ang < person[1]:
+					self.target_readings[ang] = dist
+			self.readings_list = self.target_readings.items()
+			self.readings_list = sorted(self.readings_list, key=lambda tup: tup[0])
+			count = -1
+			comparecount = 0
+			target = None
+			comparison = self.readings_list[0]
+			for angle, distance in self.readings_list:
+				if abs(distance - comparison[1]) < 2:
+					count += 1
+				else:
+					if count > comparecount:
+						comparecount = count
+						target = comparison
+					comparison = angle, distance
+					count = 0
+
+			self.targeted.append(target[0] + count/2, target[1])
+
+		return self.targeted
+
+
+if __name__ == "__main__":
+
+
+	Bodies = BodyDetector()
+	Targeter = TargetLocator()
+
+	while True:
+
+
+		crowd = Bodies.find_bodies()
+
+		# Targeter.track(crowd)
+
 		k = cv2.waitKey(30) & 0xff
 
 		if k == 27:
 
 			break
-
-
 
 	cap.release()
 	cv2.destroyAllWindows()
